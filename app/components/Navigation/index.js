cdd3ac16a832bd9a3c5afb58faf988e6b8024eb8
manishsaraan <manish021js@gmail.com>
2017-12-24 18:13:01 -0800

    Drawer component added

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -6,18 +6,21 @@
 
 import React from 'react';
 import AppBar from '../AppBar';
+import Drawer from '../Drawer';
 import styles from './styles.css';
 
-function Navigation({ topics, selectTopic, toggleDrawer }) {
- const topicNodes = topics.map( t => (
-      <div key={t.name} onClick={ () => selectTopic(t) }>
-       	 {t.name}
-      </div>
- 	));
+function Navigation({ topics, selectTopic, toggleDrawer , isDrawerOpen}) {
+
   return (
     <div className={styles.navigation}>
      <AppBar toggleDrawer = {toggleDrawer}/>
-     {topicNodes}
+     <Drawer 
+       	items  = {topics}
+		selectItem = {selectTopic}
+		itemLabelAttr = "name"
+		itemKeyAttr = "name"
+		isDrawerOpen = { isDrawerOpen }     
+     />     
     </div>
   );
 }
@@ -31,5 +34,6 @@ Navigation.propTypes = {
 		).isRequired,
 	selectTopic : React.PropTypes.func.isRequired,
 	toggleDrawer : React.PropTypes.func.isRequired,
+	isDrawerOpen : React.PropTypes.bool.isRequired,
 }
 export default Navigation;
